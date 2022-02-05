c806ec6f0a9cc054eac0e679f48a39cc10fcbaf3
Alex Cervantes <thealejandrocervantes@gmail.com>
2022-02-04 21:21:04 -0500

    Mobile fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-setTimeout(function(){
-    $("#iam").addClass("swe-shine");
-    setTimeout(function(){
-        $("#iam").removeClass("swe-shine");
-    },1000);
-}, 2500);
+if ($(window).width() <= 428) {
+	$("div.firstAnimation").replaceWith(
+		'<h1 id="MyName">ALEJANDRO CERVANTES</h1>'
+	);
+}
+
+
+
+setTimeout(function () {
+	$("#iam").addClass("swe-shine");
+	setTimeout(function () {
+		$("#iam").removeClass("swe-shine");
+	}, 1000);
+}, 2000);
